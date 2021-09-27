99711438edc74a57fd99baf5d10c787337f50833
weishuaiqiang <wei19966524253@outlook.com>
2021-09-27 17:09:16 +0800

    增加了使用位运算进行数值交换

--- src/com/wsq/demo01/BitwiseOperation.java
+++ src/com/wsq/demo01/BitwiseOperation.java
@@ -53,7 +53,30 @@ public class BitwiseOperation {
         // 0000 0000 0000 0000 0000 0000 0000 1100
         // 也就是12
 
-        // 5.
+        // 5. 练习：交换两个变量的值
+        int num1 = 10;
+        int num2 = 20;
+        System.out.println("num1 = " + num1 + ",num2 = " + num2);
+        // 方式一：定义临时变量的方式
+        // 推荐的方式
+        int temp = num1;
+        num1 = num2;
+        num2 = temp;
+        System.out.println("num1 = " + num1 + ",num2 = " + num2);
+        // 方式二：好处：不用定义临时变量
+        // 缺点 1.相加操作可能超出存储范围，溢出
+        // 2.有局限性
+        num1 = num1 + num2;
+        num2 = num1 - num2;
+        num1 = num1 - num2;
+        System.out.println("num1 = " + num1 + ",num2 = " + num2);
+        // 方式三：使用位运算符
+        // 有局限性：只能适用于数值类型
+        num1 = num1 ^ num2;
+        num2 = num1 ^ num2; // 此时num2已经等于原来的num1
+        num1 = num1 ^ num2; // 此时num1已经等于原来的num2
+        System.out.println("num1 = " + num1 + ",num2 = " + num2);
+
 
     }
 }
